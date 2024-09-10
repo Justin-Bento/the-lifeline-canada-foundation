35ba960f99095d208f425987af0715d7a272c23a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-10 16:46:47 -0400

    Added main text for home page.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.display_title}>The LifeLine Canada Foundation</Text>
+      <Text>“Mental Health Resources at Your Fingertips</Text>
+      <Button title="Continue" />
       <StatusBar style="auto" />
     </View>
   );
