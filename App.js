264d2577dd5a1c0965d7f9c1145c942b32d80bb9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-15 12:02:49 -0400

    Removed Old Layout to follow react native tutorial.

--- App.js
+++ App.js
@@ -1,60 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./pages/HomeScreen";
+import DetailsScreen from "./pages/DetailsScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.display_title}>The LifeLine Canada Foundation</Text>
-      <Text style={styles.large_body}>“Mental Health Resources at Your Fingertips</Text>
-      <Button title="Continue" />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-function Button(props) {
-  const { onPress, title = 'Save' } = props;
-  return (
-    <Pressable style={styles.button} onPress={onPress}>
-      <Text style={styles.text}>{title}</Text>
-    </Pressable>
-  );
-}
-
-const styles = StyleSheet.create({
-  display_title: {
-    fontSize: 36,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  large_body: {
-    marginTop: 16,
-    marginBottom: 16,
-    fontSize: 20,
-    textAlign: "center"
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: '#a21caf',
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  },
-
-});
