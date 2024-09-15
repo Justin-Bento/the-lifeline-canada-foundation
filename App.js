7a42244b22d9d5f159ef7b05a2d7cf2dbf3c8ac9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-15 12:06:49 -0400

    Imported bottom navigation into main application.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./pages/HomeScreen";
 import DetailsScreen from "./pages/DetailsScreen";
+import BottomNavigation from "./components/BottomNavigation.js";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
+      <BottomNavigation />
     </NavigationContainer>
   );
 }
