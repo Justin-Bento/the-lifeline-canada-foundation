cc893bd5145ead40404d2777b9725ca68d565188
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-16 11:43:15 -0400

    Formatted document with prettier to enable proper format.

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 
 export default function BottomNavigation() {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+      <TouchableOpacity onPress={() => navigation.navigate("Home")}>
         <Text style={styles.icons}>H</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Call')}>
+      <TouchableOpacity onPress={() => navigation.navigate("Call")}>
         <Text style={styles.icons}>C</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('GetHelp')}>
+      <TouchableOpacity onPress={() => navigation.navigate("GetHelp")}>
         <Text style={styles.icons}>G</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Resource')}>
+      <TouchableOpacity onPress={() => navigation.navigate("Resource")}>
         <Text style={styles.icons}>R</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('PatternInterrupt')}>
+      <TouchableOpacity onPress={() => navigation.navigate("Pattern")}>
         <Text style={styles.icons}>P</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+      <TouchableOpacity onPress={() => navigation.navigate("Details")}>
         <Text style={styles.icons}>M</Text>
       </TouchableOpacity>
     </View>
@@ -33,15 +33,15 @@ const styles = StyleSheet.create({
     flex: 0,
     width: "100%",
     flexDirection: "row",
-    justifyContent: "space-evenly",  // Changed from alignContent to justifyContent
-    paddingVertical: 30
+    justifyContent: "space-evenly", // Changed from alignContent to justifyContent
+    paddingVertical: 30,
   },
   icons: {
     padding: 10,
     paddingHorizontal: 14,
     fontSize: 16,
-    borderWidth: 1,        // Adds border width
-    borderColor: 'black',  // Adds border color
-    borderRadius: 4
+    borderWidth: 1, // Adds border width
+    borderColor: "black", // Adds border color
+    borderRadius: 4,
   },
 });
