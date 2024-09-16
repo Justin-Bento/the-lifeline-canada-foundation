96f1f91a3183eacb330cad6415bee0d465de95d3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-16 12:27:23 -0400

    Replaced the bottom navigation with content from the app

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,12 +1,33 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate("Details")}
-      />
+    <View style={styles.container}>
+      <Text style={styles.box}>Section #1</Text>
+      <Text style={styles.box}>Section #1</Text>
+      <Text style={styles.box}>Section #1</Text>
+      <Text style={styles.box}>Section #1</Text>
+      <Text style={styles.box}>Section #1</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { 
+    width: "90%",
+    marginHorizontal: "auto",
+    flex: 1, 
+    flexDirection: "column",
+    gap: 12,
+    alignItems: "center", 
+    justifyContent: "center",
+  },
+  box: {
+    width: "100%",
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+    color: "#fff",
+    borderRadius: 32,
+    backgroundColor: "red",
+  }
+});
