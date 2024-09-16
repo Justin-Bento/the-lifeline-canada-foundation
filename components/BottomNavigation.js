f3e4a02cb5123b01510162ebe9a8d793c75c6636
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-16 10:51:15 -0400

    Applied scoped based styles to the bottom navigation.

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function BottomNavigation() {
   return (
-    <>
-      <Text>1</Text>
-      <Text>2</Text>
-      <Text>3</Text>
-      <Text>4</Text>
-      <Text>5</Text>
-      <Text>6</Text>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.button_icons}>1</Text>
+      <Text style={styles.button_icons}>2</Text>
+      <Text style={styles.button_icons}>3</Text>
+      <Text style={styles.button_icons}>4</Text>
+      <Text style={styles.button_icons}>5</Text>
+      <Text style={styles.button_icons}>6</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",  // Changed from alignContent to justifyContent
+    paddingVertical: 30
+  },
+  button_icons: {
+    padding: 8,
+    paddingHorizontal: 12,
+    fontSize: 14,
+    borderWidth: 1,        // Adds border width
+    borderColor: 'black',  // Adds border color
+    borderRadius: 4
+  },
+});
