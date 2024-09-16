ca305489e10705192a3c6e9bdba1f4ccf66caf53
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-16 10:58:31 -0400

    Updated to numbers and replace it with letters.

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -4,12 +4,12 @@ import { View, Text, StyleSheet } from "react-native";
 export default function BottomNavigation() {
   return (
     <View style={styles.container}>
-      <Text style={styles.icons}>1</Text>
-      <Text style={styles.icons}>2</Text>
-      <Text style={styles.icons}>3</Text>
-      <Text style={styles.icons}>4</Text>
-      <Text style={styles.icons}>5</Text>
-      <Text style={styles.icons}>6</Text>
+      <Text style={styles.icons}>H</Text>
+      <Text style={styles.icons}>C</Text>
+      <Text style={styles.icons}>G</Text>
+      <Text style={styles.icons}>R</Text>
+      <Text style={styles.icons}>P</Text>
+      <Text style={styles.icons}>M</Text>
     </View>
   );
 }
