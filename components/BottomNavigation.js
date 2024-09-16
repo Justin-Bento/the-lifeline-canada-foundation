9a79c8c97d2279bdb8790793154893798934818a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-16 10:31:51 -0400

    Added 6 text Items representing buttons for the bottom navigation.

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from "react";
+import { Text } from "react-native";
 
 export default function BottomNavigation() {
   return (
-    <Text>BottomNavigation</Text>
-  )
+    <>
+      <Text>1</Text>
+      <Text>2</Text>
+      <Text>3</Text>
+      <Text>4</Text>
+      <Text>5</Text>
+      <Text>6</Text>
+    </>
+  );
 }
