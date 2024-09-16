1c96c1092df152d815362b6ccbacf00a0ce5c030
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-16 11:41:20 -0400

    Imported all pages into main app.

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./pages/HomeScreen";
+import CallScreen from "./pages/CallScreen.js";
+import GetHelpScreen from "./pages/HelpScreen";
+import ResourceScreen from "./pages/ResourceScreen";
+import PatternInterruptScreen from "./pages/PatternInterruptScreen.js";
 import DetailsScreen from "./pages/DetailsScreen";
 import BottomNavigation from "./components/BottomNavigation.js";
 
@@ -12,6 +16,10 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Call" component={CallScreen} />
+        <Stack.Screen name="GetHelp" component={GetHelpScreen} />
+        <Stack.Screen name="Resource" component={ResourceScreen} />
+        <Stack.Screen name="PatternInterrupt" component={PatternInterruptScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
       <BottomNavigation />
