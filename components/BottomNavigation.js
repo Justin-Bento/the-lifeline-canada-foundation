6feffd57496ade2f792031a41902f0320726e596
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-16 11:40:55 -0400

    Implemented TouchableOpacity to enable users to switch between pages. at the bottom

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
 export default function BottomNavigation() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Text style={styles.icons}>H</Text>
-      <Text style={styles.icons}>C</Text>
-      <Text style={styles.icons}>G</Text>
-      <Text style={styles.icons}>R</Text>
-      <Text style={styles.icons}>P</Text>
-      <Text style={styles.icons}>M</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.icons}>H</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Call')}>
+        <Text style={styles.icons}>C</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('GetHelp')}>
+        <Text style={styles.icons}>G</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Resource')}>
+        <Text style={styles.icons}>R</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('PatternInterrupt')}>
+        <Text style={styles.icons}>P</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+        <Text style={styles.icons}>M</Text>
+      </TouchableOpacity>
     </View>
   );
 }
