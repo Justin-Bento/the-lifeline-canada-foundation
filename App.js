8aebe5fdf0c775f79d8d69cb4f81d28755fa9f8b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-18 23:04:29 -0400

    Added about page import  to main application.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./pages/HomeScreen";
+import AboutScreen from "./pages/AboutScreen.js";
 import CallScreen from "./pages/CallScreen.js";
 import GetHelpScreen from "./pages/HelpScreen";
 import ResourceScreen from "./pages/ResourceScreen";
@@ -15,6 +16,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AboutTCL" component={AboutScreen} />
         <Stack.Screen name="Call" component={CallScreen} />
         <Stack.Screen name="GetHelp" component={GetHelpScreen} />
         <Stack.Screen name="Resource" component={ResourceScreen} />
