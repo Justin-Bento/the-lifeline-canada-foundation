56b78d0c7a59e0bee38e9927b72f71e782065a2b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-04 16:00:07 -0400

    Updated styles accordingly

--- pages/AboutScreen.js
+++ pages/AboutScreen.js
@@ -4,11 +4,11 @@ import externalStyleSheet from "../styles/styles";
 
 export default function AboutScreen() {
   return (
-    <View style={externalStyleSheet.container}>
+    <View style={externalStyleSheet.main_container}>
       <Pressable style={externalStyleSheet.button}>
         <Text style={externalStyleSheet.body2 && externalStyleSheet.button_text}>View Our Support Services</Text>
       </Pressable>
-      <Text style={externalStyleSheet.headline}>About Screen</Text>
+      <Text style={externalStyleSheet.h4}>About Screen</Text>
       <Text style={externalStyleSheet.body1}>
         The LifeLine Canada Foundation (TLC) is a registered non-profit committed to the prevention of suicide and
         positive mental health in Canada and Worldwide. TLC has made work in this area a top priority. We envision a
@@ -21,17 +21,17 @@ export default function AboutScreen() {
         self management program for mental health struggles developed in collaboration with mental health professionals,
         expansion of TLC&#39;s Companion Paws website as well as further expansion into animal assisted therapy.
       </Text>
-      <Text style={externalStyleSheet.headline}>Mission Statement</Text>
+      <Text style={externalStyleSheet.h4}>Mission Statement</Text>
       <Text style={externalStyleSheet.body1}>
         To aid in suicide reduction across Canada by raising awareness of risk factors, providing access to online
         resources options, and promoting local programs to build mental health resilience for all!
       </Text>
-      <Text style={externalStyleSheet.headline}>Vision Statement</Text>
+      <Text style={externalStyleSheet.h4}>Vision Statement</Text>
       <Text style={externalStyleSheet.body1}>
         To continue to lower the risk of suicide to where it is a rare event across the country and to build mental
         health resilience for all.
       </Text>
-      <Text style={externalStyleSheet.headline}>Values Statement</Text>
+      <Text style={externalStyleSheet.h4}>Values Statement</Text>
       <Text style={externalStyleSheet.body1}>
         To assist in reducing the stigma around mental health conditions, and to promote evidence based resources and
         programs with integrity, respect and inclusion.
