b6952782ae236f44e83859a804be609e84e21a01
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-10 19:55:37 -0400

    Added Crisis Card to the main page

--- pages/CallScreen.js
+++ pages/CallScreen.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { externalStyleSheet, Typography, Layouts } from "../styles/styles";
+import { View, Text, Image } from "react-native";
+import { Typography, Layouts } from "../styles/styles";
+import Card from "../components/Card";
 
 export default function CallScreen() {
   return (
     <>
-      <View style={Layouts.container}></View>
+      <View style={Layouts.container}>
+        <Card style={Layouts.cardLayout}>
+          <View style={{ marginBottom: 20 }}>
+            <View style={[{ paddingHorizontal: 24 }]}>
+              <Text style={[Typography.title_lg, { textTransform: "capitalize", textAlign: "center" }]}>
+                If you, or Someone you know is in Crisis, Call 911 Immediately!
+              </Text>
+              <Text style={(Typography.body1, { marginTop: 8, textAlign: "center" })}>
+                The LifeLine Canada is Not a Crisis Hotline.
+              </Text>
+            </View>
+            <Image
+              source={require("../assets/card_image/cbef1c0010018ea502ee11746ce1486b.png")}
+              style={Layouts.mediaImage} // Adjust these values as needed
+            />
+          </View>
+        </Card>
+      </View>
     </>
   );
 }
