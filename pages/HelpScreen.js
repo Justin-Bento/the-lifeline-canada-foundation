39bae5b8e092195b1f844eee5e6ce7e8c253f8c9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-17 12:34:02 -0400

    Created the basis of a card

--- pages/HelpScreen.js
+++ pages/HelpScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { View, ScrollView, Text } from "react-native";
 import { Typography, Layouts } from "../styles/styles";
 
 export default function HelpScreen() {
@@ -10,6 +10,21 @@ export default function HelpScreen() {
         If in crisis, call 911 or the Mental Health Crisis Lines provided below.
         The LifeLine Canada Foundation is not a crisis hotline.
       </Text>
+      <View
+        style={[
+          { backgroundColor: "#fff" },
+          { padding: 20 },
+          { marginVertical: 20 },
+          { borderWidth: 1 },
+          { borderColor: "#e4e4e7" },
+          { borderRadius: 5 },
+        ]}
+      >
+        <Text style={[Typography.body2]}>Numbers of Support</Text>
+        <Text style={[Typography.titleXL, { marginTop: 4 }]}>
+          Canada Crisis Center
+        </Text>
+      </View>
     </ScrollView>
   );
 }
