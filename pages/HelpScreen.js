99e89ce52371dd5bcff75f2609d985abb00ea79c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-17 12:28:41 -0400

    Removed the default button navigation

--- pages/HelpScreen.js
+++ pages/HelpScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { ScrollView, Text } from "react-native";
+import { Typography, Layouts } from "../styles/styles";
 
-export default function HelpScreen({ navigation }) {
+export default function HelpScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-    </View>
+    <ScrollView style={Layouts.mainContainer}>
+      <Text style={[Typography.h2, { marginBottom: 8 }]}>Get Help</Text>
+      <Text style={Typography.body1}>
+        If in crisis, call 911 or the Mental Health Crisis Lines provided below.
+        The LifeLine Canada Foundation is not a crisis hotline.
+      </Text>
+    </ScrollView>
   );
 }
