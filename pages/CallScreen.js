24782a69dcf8c0d70e264d71ee4871853a40ebbb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-17 16:24:51 -0400

    Mapped charities for the get help section.

--- pages/CallScreen.js
+++ pages/CallScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text, ScrollView, Image } from "react-native";
+import { getHelp } from "../utils/charities.json";
 
 export default function CallScreen() {
   return (
-    <ScrollView className="w-11/12 mx-auto my-8">
+    <ScrollView className="w-11/12 mx-auto py-10">
       <View className="overflow-hidden bg-white shadow sm:rounded-lg">
         <View className="px-4 py-5 sm:p-6">
           <Text className="text-lg font-bold text-center">
@@ -19,20 +20,22 @@ export default function CallScreen() {
         </View>
       </View>
       {/* End of the 911 Card */}
-      <View className="overflow-hidden rounded-lg bg-white shadow-sm mt-10">
-        <View className="px-4 py-5 sm:p-6 w-full aspect-[16/8] bg-gray-300 flex items-center justify-center">
-          <Text>Visual Media Is Here</Text>
+      {getHelp.map((charity, index) => (
+        <View
+          key={index} // Unique key for each element
+          className="overflow-hidden rounded-lg bg-white shadow-sm mt-10"
+        >
+          <View className="px-4 py-5 sm:p-6 w-full aspect-[16/8] bg-gray-300 flex items-center justify-center">
+            <Text>Visual Media Is Here</Text>
+          </View>
+          <View className="bg-white px-4 py-4 sm:px-6">
+            <Text className="text-xl font-semibold capitalize mb-1">
+              {charity.title}
+            </Text>
+            <Text className="text-sm">{charity.description}</Text>
+          </View>
         </View>
-        <View className="bg-white px-4 py-4 sm:px-6">
-          <Text className="text-xl font-semibold capitalize mb-1">
-            Content goes here
-          </Text>
-          <Text className="text-sm">
-            We use less vertical padding on card footers at all sizes than on
-            headers or body sections
-          </Text>
-        </View>
-      </View>
+      ))}
     </ScrollView>
   );
 }
