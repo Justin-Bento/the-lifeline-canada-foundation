5fc97775ad242b477aa3a7ec3a08941fdafbeecb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-17 13:32:07 -0400

    Updated styles to use tailwind css classes

--- pages/AboutScreen.js
+++ pages/AboutScreen.js
@@ -6,51 +6,55 @@ import { Button } from "../components/Button";
 
 export default function AboutScreen() {
   return (
-    <ScrollView>
-      <View style={[Layouts.mainContainer, Layouts.flexLeftAlign]}>
-        <Card style={Layouts.cardLayout}>
-          <View style={{ marginBottom: 20 }}>
-            <Text style={[Typography.titleLG, { textTransform: "capitalize", textAlign: "center" }]}>
-              If you, or Someone you know is in Crisis, Call 911 Immediately!
-            </Text>
-            <Text style={(Typography.body1, { marginTop: 8, textAlign: "center" })}>
-              The LifeLine Canada is Not a Crisis Hotline.
-            </Text>
-            <Image
-              source={require("../assets/card_image/cbef1c0010018ea502ee11746ce1486b.png")}
-              style={Layouts.mediaImage} // Adjust these values as needed
-            />
-          </View>
-        </Card>
-        <Text style={Typography.h4}>About Screen</Text>
-        <Text style={Typography.body1}>
-          The LifeLine Canada Foundation (TLC) is a registered non-profit committed to the prevention of suicide and
-          positive mental health in Canada and Worldwide. TLC has made work in this area a top priority. We envision a
-          country where suicide is a rare event, where people are nurtured and supported, where individuals and families
-          are aware of the risk factors for suicide and actively seek help from accessible, effective community
-          resources.
+    <ScrollView className="my-8">
+      <View className="w-11/12 mx-auto space-y-4">
+        <View className="bg-white shadow-sm rounded-md p-4 border border-gray-300">
+          <Text className="text-lg font-bold text-center">
+            If you, or Someone you know is in Crisis, Call 911 Immediately!
+          </Text>
+          <Text className="text-base/7 text-center mt-2">
+            The LifeLine Canada is Not a Crisis Hotline.
+          </Text>
+          <Image
+            source={require("../assets/card_image/cbef1c0010018ea502ee11746ce1486b.png")}
+            style={Layouts.mediaImage} // Adjust these values as needed
+          />
+        </View>
+        <Text className="text-2xl font-medium">About Screen</Text>
+        <Text className="text-md leading-5">
+          The LifeLine Canada Foundation (TLC) is a registered non-profit
+          committed to the prevention of suicide and positive mental health in
+          Canada and Worldwide. TLC has made work in this area a top priority.
+          We envision a country where suicide is a rare event, where people are
+          nurtured and supported, where individuals and families are aware of
+          the risk factors for suicide and actively seek help from accessible,
+          effective community resources.
         </Text>
-        <Text style={Typography.body1}>
-          We have several other life changing initiatives coming soon in 2023! Some of the activities include the
-          expansion of The LifeLine App, expansion of TLC&#39;s website, resources and services, a human approach online
-          self management program for mental health struggles developed in collaboration with mental health
-          professionals, expansion of TLC&#39;s Companion Paws website as well as further expansion into animal assisted
-          therapy.
+        <Text className="text-md leading-5">
+          We have several other life changing initiatives coming soon in 2023!
+          Some of the activities include the expansion of The LifeLine App,
+          expansion of TLC&#39;s website, resources and services, a human
+          approach online self management program for mental health struggles
+          developed in collaboration with mental health professionals, expansion
+          of TLC&#39;s Companion Paws website as well as further expansion into
+          animal assisted therapy.
         </Text>
-        <Text style={Typography.h4}>Mission Statement</Text>
-        <Text style={Typography.body1}>
-          To aid in suicide reduction across Canada by raising awareness of risk factors, providing access to online
-          resources options, and promoting local programs to build mental health resilience for all!
+        <Text className="text-2xl font-medium">Mission Statement</Text>
+        <Text className="text-md leading-5">
+          To aid in suicide reduction across Canada by raising awareness of risk
+          factors, providing access to online resources options, and promoting
+          local programs to build mental health resilience for all!
         </Text>
-        <Text style={Typography.h4}>Vision Statement</Text>
-        <Text style={Typography.body1}>
-          To continue to lower the risk of suicide to where it is a rare event across the country and to build mental
-          health resilience for all.
+        <Text className="text-2xl font-medium">Vision Statement</Text>
+        <Text className="text-md leading-5">
+          To continue to lower the risk of suicide to where it is a rare event
+          across the country and to build mental health resilience for all.
         </Text>
-        <Text style={Typography.h4}>Values Statement</Text>
-        <Text style={Typography.body1}>
-          To assist in reducing the stigma around mental health conditions, and to promote evidence based resources and
-          programs with integrity, respect and inclusion.
+        <Text className="text-2xl font-medium">Values Statement</Text>
+        <Text className="text-md leading-5">
+          To assist in reducing the stigma around mental health conditions, and
+          to promote evidence based resources and programs with integrity,
+          respect and inclusion.
         </Text>
       </View>
     </ScrollView>
