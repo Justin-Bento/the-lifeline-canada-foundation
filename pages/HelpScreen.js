b7686767cd43fa8375314382bab92ab19a5980d3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-25 11:08:23 -0400

    Created a section for pressable filters

--- pages/HelpScreen.js
+++ pages/HelpScreen.js
@@ -1,30 +1,30 @@
 import React from "react";
 import { View, ScrollView, Text } from "react-native";
-import { Typography, Layouts } from "../styles/styles";
 
 export default function HelpScreen() {
   return (
-    <ScrollView style={Layouts.mainContainer}>
-      <Text style={[Typography.h2, { marginBottom: 8 }]}>Get Help</Text>
-      <Text style={Typography.body1}>
-        If in crisis, call 911 or the Mental Health Crisis Lines provided below.
-        The LifeLine Canada Foundation is not a crisis hotline.
-      </Text>
-      <View
-        style={[
-          { backgroundColor: "#fff" },
-          { padding: 20 },
-          { marginVertical: 20 },
-          { borderWidth: 1 },
-          { borderColor: "#e4e4e7" },
-          { borderRadius: 5 },
-        ]}
-      >
-        <Text style={[Typography.body2]}>Numbers of Support</Text>
-        <Text style={[Typography.titleXL, { marginTop: 4 }]}>
-          Canada Crisis Center
+    <ScrollView className="w-11/12 pt-8 mx-auto">
+      <View className="space-y-2">
+        <Text className="text-3xl font-medium tracking-tight line-clamp-2">
+          Get Help
+        </Text>
+        <Text className="text-base leading-6">
+          If in crisis, call 911 or the Mental Health Crisis Lines provided
+          below. The LifeLine Canada Foundation is not a crisis hotline.
         </Text>
       </View>
+      {/* End of Headline */}
+      <View className="flex flex-row">
+        <View className="flex flex-col items-center">
+          <View className="block bg-purple-300 w-20 h-20 rounded-full"></View>
+          <Text className="p-10">Phase Chat Line</Text>
+        </View>
+        <View className="flex flex-col items-center">
+          <View className="block bg-purple-300 w-20 h-20 rounded-full"></View>
+          <Text className="p-10">Phase Chat Line</Text>
+        </View>
+      </View>
+      {/* End of Pressable Filters Headline */}
     </ScrollView>
   );
 }
