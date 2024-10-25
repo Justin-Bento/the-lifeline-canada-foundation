ead6ad11958290f0df69d6d29fb779037c385f76
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-25 12:01:27 -0400

    Created the title for the resources page

--- pages/ResourceScreen.js
+++ pages/ResourceScreen.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
-export default function ResourceScreen({ navigation }) {
+export default function ResourceScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-    </View>
+    <ScrollView className="w-11/12 mx-auto pt-6">
+      <View className="space-y-2">
+        <Text className="scroll-m-20 text-3xl font-semibold tracking-tight">
+          Resources
+        </Text>
+        <Text className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+          natus perferendis rerum nobis fugiat eum dolor dolorem, exercitationem
+          nostrum aperiam, quos odit in, necessitatibus dolorum odio porro
+          deserunt delectus qui.
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
