39fc6fd4bf06492616388704399224b28a663058
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-25 12:04:59 -0400

    Moved content into it's own component

--- pages/ResourceScreen.js
+++ pages/ResourceScreen.js
@@ -1,20 +1,14 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import { ScrollView } from "react-native";
+import Headline from "../components/Headline";
 
 export default function ResourceScreen() {
   return (
     <ScrollView className="w-11/12 mx-auto pt-6">
-      <View className="space-y-2">
-        <Text className="scroll-m-20 text-3xl font-semibold tracking-tight">
-          Resources
-        </Text>
-        <Text className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          natus perferendis rerum nobis fugiat eum dolor dolorem, exercitationem
-          nostrum aperiam, quos odit in, necessitatibus dolorum odio porro
-          deserunt delectus qui.
-        </Text>
-      </View>
+      <Headline
+        title="Resources"
+        supporting="This app provides information about anxiety, depression and suicide. Online resources alone are no substitute for professional medical advice, diagnosis or treatment. Seek the advice of an appropriately qualified healthcare professional before making decisions about your circumstances."
+      />
     </ScrollView>
   );
 }
