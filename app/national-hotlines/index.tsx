50dac9f67b4aa999cd81d8199d2f7ab7360cdd1b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-26 17:23:56 -0400

    Updated content to ensure it's up to date.

--- app/national-hotlines/index.tsx
+++ app/national-hotlines/index.tsx
@@ -1,9 +1,39 @@
-import { ScrollView, Text } from "react-native";
+import React from "react";
+import { View, Text, ScrollView, Image, FlatList } from "react-native";
+import { NationalHotlines } from "@/assets/content";
+import ContentCard from "@/components/ContentCard";
+import CallImmediately from "@/components/CallImmediately";
 
-export default function GetSupportPage() {
+// function getImagePath(path: string): string | null {
+//   return images[path] || null;
+// }
+
+export default function NationalHotlineScreen() {
   return (
-    <ScrollView className="w-11/12 mx-auto">
-      <Text>Welcome, National Hotlines page!</Text>
+    <ScrollView className="wrapper">
+      <View className="mb-7">
+        <Text className="text-4xl font-medium text-start">
+          National Hotlines
+        </Text>
+        <Text className="text-base font-medium text-start mt-2">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, rem
+          corrupti impedit eos officia illo sit debitis blanditiis.
+        </Text>
+      </View>
+      <CallImmediately />
+      {/* End of the 911 Card */}
+      <FlatList
+        data={NationalHotlines}
+        keyExtractor={(item) => item.headline}
+        renderItem={({ item }) => (
+          <ContentCard
+            key={item.headline}
+            title={item.headline}
+            supporting={item.overview}
+            image={`/assets/images/${item.media}`}
+          />
+        )}
+      />
     </ScrollView>
   );
 }
