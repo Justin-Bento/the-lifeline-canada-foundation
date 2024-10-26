e2f9f6844fb68b811bfcdd88a58652f66c6fe846
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-25 20:46:17 -0400

    Transferred daily quotes to content.js file

--- utils/content.js
+++ utils/content.js
@@ -215,6 +215,148 @@ const PatternInterrupts = [
     overview: "Inspirational Quotes.",
   },
 ];
+const dailyQuotes = [
+  {
+    id: 1,
+    quote: "Life is about making an impact, not making an income.",
+    author: "Kevin Kruse",
+  },
+  {
+    id: 2,
+    quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+    author: "Napoleon Hill",
+  },
+  {
+    id: 3,
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 4,
+    quote:
+      "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+    author: "Robert Frost",
+  },
+  {
+    id: 5,
+    quote: "I attribute my success to this: I never gave or took any excuse.",
+    author: "Florence Nightingale",
+  },
+  {
+    id: 6,
+    quote: "You miss 100% of the shots you don’t take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    id: 7,
+    quote:
+      "The most difficult thing is the decision to act, the rest is merely tenacity",
+    author: "Amelia Earhart",
+  },
+  {
+    id: 8,
+    quote: "Every strike brings me closer to the next home run",
+    author: "Babe Ruth",
+  },
+  {
+    id: 9,
+    quote: "Definiteness of purpose is the starting point of all achievement",
+    author: "W. Clement Stone",
+  },
+  {
+    id: 10,
+    quote: "Life isn’t about getting and having, it’s about giving and being.",
+    author: "Kevin Kruse",
+  },
+  {
+    id: 11,
+    quote: "Life is what happens to you while you’re busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    id: 12,
+    quote: "We become what we think about",
+    author: "Earl Nightingale",
+  },
+  {
+    id: 13,
+    quote:
+      "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover",
+    author: "Mark Twain",
+  },
+  {
+    id: 14,
+    quote: "Life is 10% what happens to me and 90% of how I react to it",
+    author: "Charles Swindoll",
+  },
+  {
+    id: 15,
+    quote:
+      "The most common way people give up their power is by thinking they don’t have any",
+    author: "Alice Walker",
+  },
+  {
+    id: 16,
+    quote: "The mind is everything. What you think you become.",
+    author: "Buddha",
+  },
+  {
+    id: 17,
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now",
+    author: "Chinese Proverb",
+  },
+  {
+    id: 18,
+    quote: "An unexamined life is not worth living",
+    author: "Socrates",
+  },
+  {
+    id: 19,
+    quote: "Eighty percent of success is showing up",
+    author: "Woody Allen",
+  },
+  {
+    id: 20,
+    quote:
+      "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 21,
+    quote: "Winning isn’t everything, but wanting to win is",
+    author: "Vince Lombardi",
+  },
+  {
+    id: 21,
+    quote:
+      "I am not a product of my circumstances. I am a product of my decisions",
+    author: "Stephen Covey",
+  },
+  {
+    id: 23,
+    quote:
+      "Every child is an artist. The problem is how to remain an artist once he grows up",
+    author: "Pablo Picasso",
+  },
+  {
+    id: 24,
+    quote:
+      "You can never cross the ocean until you have the courage to lose sight of the shore",
+    author: "Christopher Columbus",
+  },
+  {
+    id: 25,
+    quote:
+      "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel",
+    author: "Maya Angelou",
+  },
+
+  // {
+  //   quote: "",
+  //   author: "",
+  // },
+];
 // Export the resources array
 export {
   HomeScreenContent,
@@ -222,4 +364,5 @@ export {
   NationalHotlines,
   resources,
   GetHelp,
+  dailyQuotes,
 };
