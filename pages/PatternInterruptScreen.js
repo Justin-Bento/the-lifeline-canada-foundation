594e8719f3c023de86c88d3576708c17b19967f8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-25 20:27:19 -0400

    Removed unused component

--- pages/PatternInterruptScreen.js
+++ pages/PatternInterruptScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View } from "react-native";
 import Headline from "../components/Headline";
 
 export default function PatternInterruptScreen() {
