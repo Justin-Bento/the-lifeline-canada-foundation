6581d78253824ae353f517db9bfad6ac6a6e9e2e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-25 20:35:35 -0400

    Applied content to a Flat List and grabbed data from content file to represent a content management system.

--- pages/PatternInterruptScreen.js
+++ pages/PatternInterruptScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, FlatList } from "react-native";
 import Headline from "../components/Headline";
+import ContentCard from "../components/ContentCard";
+import { PatternInterrupts } from "../utils/content";
 
 export default function PatternInterruptScreen() {
   return (
@@ -11,6 +13,14 @@ export default function PatternInterruptScreen() {
           supporting="Pattern Interrupts are a way to change a person’s pattern of thought. We all have behavior patterns that are habit sequences. Usually habits are useful. Auto pilot means our brains have become so efficient at doing something we can tune out our conscious minds which excels at running patterns or programs automatically. This sometimes can present a problem when trying to change your own or someone else’s habitual patterns of thought, emotion or behavior. It is useful to interrupt a pattern when stuck in a non resourceful state."
         />
       </View>
+      <FlatList
+        data={PatternInterrupts}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <ContentCard title={item.title} supporting={item.overview} />
+        )}
+        contentContainerStyle={{ paddingVertical: 8, gap: 8 }} // To add space between cards
+      />
     </ScrollView>
   );
 }
