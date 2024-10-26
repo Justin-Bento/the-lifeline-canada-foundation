930bdcc6fae4accd358948f00ad1bc5e3627bee5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-26 15:31:34 -0400

    Created all the pages fro the application.

---
+++ app/get-support.tsx
@@ -0,0 +1,9 @@
+import { ScrollView, Text } from "react-native";
+
+export default function GetSupportPage() {
+  return (
+    <ScrollView className="w-11/12 mx-auto">
+      <Text>Welcome, Get Support page!</Text>
+    </ScrollView>
+  );
+}
