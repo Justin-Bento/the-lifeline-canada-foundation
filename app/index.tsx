13f2e3275e2b9abe3d3938c9e278074074a0758e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-26 15:21:47 -0400

    Removed default setup from expo and add support for nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+export default function index() {
+  return (
+    <ScrollView className="">
+      <Text className="">Welcome, Home Page!</Text>
+    </ScrollView>
+  );
+}
