66827d6e0ec14ba5d59dd63de2113f2debfaacf8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-26 15:45:53 -0400

    Imported assets from main project.

---
+++ assets/content.js
@@ -0,0 +1,368 @@
+const HomeScreenContent = [
+  {
+    key: "About TLC",
+    overview: "Learn more about The Lifeline Canada Foundation.",
+    href: "AboutTCL",
+    ay13Label: "Learn more about The Lifeline Canada Foundation.",
+    ay13Role: "button",
+  },
+  {
+    key: "National Hotlines",
+    overview:
+      "See our directory of  mental health crisis support in your area.",
+    href: "National Hotlines",
+    ay13Label: "Contact national hotlines for professional support",
+    ay13Role: "button",
+  },
+  {
+    key: "Get Help",
+    overview: "Find the support you need today.",
+    href: "GetHelp",
+    ay13Label: "Find the support you need today",
+    ay13Role: "button",
+  },
+  {
+    key: "Resources",
+    overview: "Explore resources for wellness.",
+    href: "Resource",
+    ay13Label: "Explore resources for wellness",
+    ay13Role: "button",
+  },
+  {
+    key: "Pattern Interrupts",
+    overview: "Learn how to rewire your mind.",
+    href: "Pattern",
+    ay13Label: "Learn how to rewire your mind",
+    ay13Role: "button",
+  },
+  {
+    key: "More Resources",
+    overview: "Explore additional tools for mental wellness.",
+    href: "Details",
+    ay13Label: "Explore additional tools for mental wellness",
+    ay13Role: "button",
+  },
+];
+const NationalHotlines = [
+  {
+    media: "canada-suicide-prevention-service.png",
+    headline: "Canadian Crisis Center",
+    overview: "Numbers for support",
+  },
+  {
+    media: "suicide-prevention.png",
+    headline: "United States Crisis Center",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "International Crisis Center",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "International Crisis Center",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "Coping With Suicidal Thoughts",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "How can I help someone",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "Myths & Facts",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "E-Counseling",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "Self Management",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "Warning Signs",
+    overview: "Numbers for support",
+  },
+  {
+    headline: "Search for a professional",
+    overview: "Numbers for support",
+  },
+];
+const GetHelp = [
+  { headline: "Canadian Crisis Centers", overview: "Numbers for Support" },
+  { headline: "United States Crisis Centers", overview: "Numbers for Support" },
+  { headline: "International Crisis Centers", overview: "Numbers for Support" },
+  {
+    headline: "Coping with Suicidal Thoughts",
+    overview: "Find ways to cope with sadness.",
+  },
+  {
+    headline: "How can I help someone",
+    overview: "Find ways to cope with sadness.",
+  },
+  { headline: "myths & facts", overview: "Numbers for Support" },
+  { headline: "E-Counselling", overview: "Numbers for Support" },
+  { headline: "Self Management", overview: "Numbers for Support" },
+  { headline: "Warning Signs", overview: "Numbers for Support" },
+  { headline: "Search for a Professional", overview: "Numbers for Support" },
+];
+const resources = [
+  {
+    headline: "Coping With Suicidal Thoughts",
+    overview: "Find ways to cope with sadness",
+  },
+  { headline: "Search For A Professional", overview: "Numbers for Support" },
+  { headline: "Online Counselling", overview: "Numbers for Support" },
+  { headline: "Self Management", overview: "Numbers for Support" },
+  { headline: "How Can I Help Someone?", overview: "Numbers for Support" },
+  { headline: "Warning Signs", overview: "Numbers for Support" },
+  { headline: "Depression", overview: "Numbers for Support" },
+  { headline: "Myths & Facts", overview: "Numbers for Support" },
+  { headline: "Addiction", overview: "Numbers for Support" },
+  { headline: "Anxiety", overview: "Numbers for Support" },
+  {
+    headline: "Post Traumatic Stress Disorder",
+    overview: "Numbers for Support",
+  },
+  { headline: "Grief", overview: "Numbers for Support" },
+  { headline: "Teens & Youth", overview: "Numbers for Support" },
+  { headline: "Resources For Parents", overview: "Numbers for Support" },
+  { headline: "College Students", overview: "Numbers for Support" },
+  { headline: "LGBTQ2+", overview: "Numbers for Support" },
+  { headline: "Indigenous Issues", overview: "Numbers for Support" },
+  { headline: "Seniors", overview: "Numbers for Support" },
+  { headline: "Men", overview: "Numbers for Support" },
+  { headline: "Veterans & Military", overview: "Numbers for Support" },
+  { headline: "Refugees", overview: "Numbers for Support" },
+  { headline: "Attempt Survivors", overview: "Numbers for Support" },
+  { headline: "Survivors of Suicide Loss", overview: "Numbers for Support" },
+  { headline: "Bereaved Support", overview: "Numbers for Support" },
+  { headline: "After Suicide Loss", overview: "Numbers for Support" },
+  { headline: "Educators", overview: "Numbers for Support" },
+  { headline: "Health Professionals", overview: "Numbers for Support" },
+  { headline: "First Responders", overview: "Numbers for Support" },
+  { headline: "Care Givers", overview: "Numbers for Support" },
+  { headline: "Media Professionals", overview: "Numbers for Support" },
+  { headline: "Workplace Employers", overview: "Numbers for Support" },
+  { headline: "Resource Organizations", overview: "Number for Support" },
+];
+const PatternInterrupts = [
+  {
+    title: "Acts Of Kindness",
+    overview: "",
+  },
+  {
+    title: "Comedy",
+    overview: "",
+  },
+  {
+    title: "Coping Mechanisms",
+    overview: "",
+  },
+  {
+    title: "Emotional Support Videos",
+    overview: "",
+  },
+  {
+    title: "Helpful Movies",
+    overview: "",
+  },
+  {
+    title: "Image Gallery",
+    overview: "",
+  },
+  {
+    title: "Improve Your Mood",
+    overview: "",
+  },
+  {
+    title: "Inspiring Videos",
+    overview: "",
+  },
+  {
+    title: "Interactive Mind",
+    overview: "",
+  },
+  {
+    title: "Life Changing Books",
+    overview: "",
+  },
+  {
+    title: "Mental Health Apps",
+    overview: "",
+  },
+  {
+    title: "Movies of Inspiration",
+    overview: "",
+  },
+  {
+    title: "Self Management",
+    overview: "",
+  },
+  {
+    title: "Uplifting Music Videos",
+    overview: "",
+  },
+  {
+    title: "Transform Negative Thoughts",
+    overview: "",
+  },
+  {
+    title: "Words of Wisdom",
+    overview: "Inspirational Quotes.",
+  },
+];
+const dailyQuotes = [
+  {
+    id: 1,
+    quote: "Life is about making an impact, not making an income.",
+    author: "Kevin Kruse",
+  },
+  {
+    id: 2,
+    quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+    author: "Napoleon Hill",
+  },
+  {
+    id: 3,
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 4,
+    quote:
+      "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+    author: "Robert Frost",
+  },
+  {
+    id: 5,
+    quote: "I attribute my success to this: I never gave or took any excuse.",
+    author: "Florence Nightingale",
+  },
+  {
+    id: 6,
+    quote: "You miss 100% of the shots you don’t take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    id: 7,
+    quote:
+      "The most difficult thing is the decision to act, the rest is merely tenacity",
+    author: "Amelia Earhart",
+  },
+  {
+    id: 8,
+    quote: "Every strike brings me closer to the next home run",
+    author: "Babe Ruth",
+  },
+  {
+    id: 9,
+    quote: "Definiteness of purpose is the starting point of all achievement",
+    author: "W. Clement Stone",
+  },
+  {
+    id: 10,
+    quote: "Life isn’t about getting and having, it’s about giving and being.",
+    author: "Kevin Kruse",
+  },
+  {
+    id: 11,
+    quote: "Life is what happens to you while you’re busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    id: 12,
+    quote: "We become what we think about",
+    author: "Earl Nightingale",
+  },
+  {
+    id: 13,
+    quote:
+      "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover",
+    author: "Mark Twain",
+  },
+  {
+    id: 14,
+    quote: "Life is 10% what happens to me and 90% of how I react to it",
+    author: "Charles Swindoll",
+  },
+  {
+    id: 15,
+    quote:
+      "The most common way people give up their power is by thinking they don’t have any",
+    author: "Alice Walker",
+  },
+  {
+    id: 16,
+    quote: "The mind is everything. What you think you become.",
+    author: "Buddha",
+  },
+  {
+    id: 17,
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now",
+    author: "Chinese Proverb",
+  },
+  {
+    id: 18,
+    quote: "An unexamined life is not worth living",
+    author: "Socrates",
+  },
+  {
+    id: 19,
+    quote: "Eighty percent of success is showing up",
+    author: "Woody Allen",
+  },
+  {
+    id: 20,
+    quote:
+      "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs",
+  },
+  {
+    id: 21,
+    quote: "Winning isn’t everything, but wanting to win is",
+    author: "Vince Lombardi",
+  },
+  {
+    id: 21,
+    quote:
+      "I am not a product of my circumstances. I am a product of my decisions",
+    author: "Stephen Covey",
+  },
+  {
+    id: 23,
+    quote:
+      "Every child is an artist. The problem is how to remain an artist once he grows up",
+    author: "Pablo Picasso",
+  },
+  {
+    id: 24,
+    quote:
+      "You can never cross the ocean until you have the courage to lose sight of the shore",
+    author: "Christopher Columbus",
+  },
+  {
+    id: 25,
+    quote:
+      "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel",
+    author: "Maya Angelou",
+  },
+
+  // {
+  //   quote: "",
+  //   author: "",
+  // },
+];
+// Export the resources array
+export {
+  HomeScreenContent,
+  PatternInterrupts,
+  NationalHotlines,
+  resources,
+  GetHelp,
+  dailyQuotes,
+};
