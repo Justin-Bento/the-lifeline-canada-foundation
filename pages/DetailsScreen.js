a71e9523903d5ad01eb4bb23bf45e07664a18002
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-10-26 13:49:59 -0400

    Created base layout for Details Screen page

--- pages/DetailsScreen.js
+++ pages/DetailsScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { ScrollView, View, Text } from "react-native";
+import Headline from "../components/Headline";
 
-export default function DetailsScreen({ navigation }) {
+export default function DetailsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-    </View>
+    <ScrollView className="w-11/12 mx-auto pt-8 flex-1 flex-col space-y-4">
+      <Headline
+        title="Self-Care Hub"
+        supporting="This app provides information about anxiety, depression and suicide. Online resources alone are no substitute for professional medical advice, diagnosis or treatment. Seek the advice of an appropriately qualified healthcare professional before making decisions about your circumstances."
+      />
+      <View className="bg-gray-400 w-full h-[2]"></View>
+    </ScrollView>
   );
 }
