1682c30dfd3c5c148fbae084b037ce61120f7c94
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-27 14:27:02 -0400

    Created a hamburger stack for the website.

---
+++ src/components/NavigationTop.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+
+export default function NavigationTop() {
+  return (
+    <header>
+      <Card className="rounded-none">
+        <CardContent>
+          <p className="">Top Navigation</p>
+        </CardContent>
+      </Card>
+    </header>
+  );
+}
