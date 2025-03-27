88dab2dd69cb302132c0f13790ff9d0f9cc38f07
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-27 14:18:06 -0400

    deleted react native as this is project is website based

--- constants/utils.ts
+++
@@ -1,9 +0,0 @@
-export function CreateSlug(str: any) {
-  return str.toLowerCase().replace(/\s+/g, "-");
-}
-
-export function UndoSlug(slug: any) {
-  return slug
-    .replace(/-/g, " ") // Replace hyphens with spaces
-    .replace(/\b\w/g, (char: any) => char.toUpperCase()); // Capitalize each word
-}
--- types.ts
+++
@@ -1,7 +0,0 @@
-// types.ts
-export interface HomeScreenContentItem {
-  key: string;
-  overview: string;
-  href: string;
-  ay13Label: string;
-}
