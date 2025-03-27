041049f87478af692f1303036a56590e187bc2ad
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-27 18:31:26 -0400

    Updated next conig to allow images from unsplash.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["unsplash.com"], // Add unsplash.com to allowed domains
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
