615fa4f7a5e9ffaf737970efc4480d4111dbccd5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-27 14:27:57 -0400

    Moved components to global layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import NavigationBottom from "@/components/NavigationBottom";
-import NavigationTop from "@/components/NavigationTop";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <NavigationTop />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <Image
@@ -103,7 +100,6 @@ export default function Home() {
           </a>
         </footer>
       </div>
-      <NavigationBottom />
     </>
   );
 }
