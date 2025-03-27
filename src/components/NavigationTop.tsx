1fe51b23a1bb0d287baf62884a5453b7b66c3b71
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-27 14:42:52 -0400

    Created a basics navigation links

--- src/components/NavigationTop.tsx
+++ src/components/NavigationTop.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Card, CardContent } from "./ui/card";
+import { Card, CardContent, CardHeader } from "./ui/card";
+import { Button } from "./ui/button";
+import { navigationLinks } from "@/lib/data";
+import Image from "next/image";
 
 export default function NavigationTop() {
   return (
     <header>
-      <Card className="rounded-none">
-        <CardContent>
-          <p className="">Top Navigation</p>
+      <Card className="rounded-none p-0">
+        <CardHeader className="bg-purple-500 p-2 flex items-center">
+          <p className="text-white text-sm">English Support</p>
+          <p className="text-white text-sm">French Support</p>
+          <p className="text-white text-sm">Site Map</p>
+        </CardHeader>
+        <CardContent className="flex items-center justify-between pb-6">
+          <Image
+            className="dark:invert"
+            src="/media/logo/theLifeLineApp.png"
+            alt="Next.js logo"
+            width={320}
+            height={56}
+          />
+          <ul className="flex items-center">
+            {navigationLinks.map((link, index) => {
+              return (
+                <li key={index} className="">
+                  <Button variant="link">{link}</Button>
+                </li>
+              );
+            })}
+          </ul>
         </CardContent>
       </Card>
     </header>
---
+++ src/lib/data.ts
@@ -0,0 +1,10 @@
+export const navigationLinks = [
+  "About Us",
+  "In Crisis?",
+  "Mental Health Resources",
+  "Pattern Interrupts",
+  "The Lifeline App",
+  "Companion Paws",
+  "Get Involved",
+  "Contact Us",
+];
