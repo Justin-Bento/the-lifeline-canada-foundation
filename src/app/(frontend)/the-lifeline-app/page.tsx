e7e504e768b209ae9632434a77c186fed090bf58
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-31 12:51:19 -0400

    Created all the pages for the lifeline canada foundation

---
+++ src/app/(frontend)/the-lifeline-app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main className="container mx-auto min-h-dvh px-6 lg:py-24">
+      <h1 className="">Hello, The Lifeline App?</h1>
+    </main>
+  );
+}
