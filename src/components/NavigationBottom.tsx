1c49995930b303919598f7d8d191d84edc401f45
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-03-31 15:35:01 -0400

    Updated the comapny name.

--- src/components/NavigationBottom.tsx
+++ src/components/NavigationBottom.tsx
@@ -185,7 +185,7 @@ export default function NavigationBottom() {
         </div>
         <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
           <p className="text-sm/6 text-foreground/60">
-            &copy; {new Date().getFullYear()} The Lifeline Canada Foundation,
+            &copy; {new Date().getFullYear()} The LifeLine Canada Foundation,
             Inc. All rights reserved.
           </p>
         </div>
