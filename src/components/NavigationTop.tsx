8f0324103bbe0a9942d3b7ad1d7697108ff6dca9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-31 13:27:46 -0400

    Adjusted spacing for banner so it's flush on desktop and mobile screens.

--- src/components/NavigationTop.tsx
+++ src/components/NavigationTop.tsx
@@ -7,8 +7,8 @@ import { X } from "lucide-react";
 export default function NavigationTop() {
   return (
     <header>
-      <Card>
-        <CardHeader>
+      <Card className="pt-0">
+        <CardHeader className="px-0">
           <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
             <div
               aria-hidden="true"
