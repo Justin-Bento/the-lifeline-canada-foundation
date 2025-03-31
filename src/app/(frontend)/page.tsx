620f1855c3798de45018b1d60e25499147277b7c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-03-31 17:48:53 -0400

    Created the base for the products / service section

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <main className="container mx-auto min-h-dvh space-y-32 py-24  lg:px-6">
+      <main className="container mx-auto min-h-dvh space-y-64 py-24  lg:px-6">
         <section className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           <div className="relative w-full aspect-[16/10]">
             <Image
@@ -52,6 +58,83 @@ export default function Home() {
                 </Link>
               </li>
             </ul>
+          </div>
+        </section>
+
+        {/* Products / Services Section */}
+        <section className="space-y-12">
+          <span className="">
+            <p className="text-xs font-mono">What Can We Help You With?</p>
+            <h2 className="mb-10 mt-2 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+              Mental Health Resources At Your Fingertips.
+            </h2>
+          </span>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <Card className="shadow-none">
+              <CardContent className="space-y-2">
+                <CardTitle className="text-lg font-semibold">
+                  Mental Health Resources.
+                </CardTitle>
+                <CardDescription className="line-clamp-3 text-sm/6">
+                  The guides, tool kits and resources throughout this site
+                  represent a compilation of curated suicide prevention
+                  resources from various sources around the world. When clicking
+                  3rd party links, you will be redirected to websites not
+                  affiliated with, but supported by The LifeLine Canada
+                  Foundation.
+                </CardDescription>
+              </CardContent>
+            </Card>
+            <Card className="shadow-none">
+              <CardContent className="space-y-2">
+                <CardTitle className="text-lg font-semibold">
+                  Pattern Intterupts
+                </CardTitle>
+                <CardDescription className="line-clamp-3 text-sm/6">
+                  A Pattern Interrupt is a way to alter our mental, emotional,
+                  or behavioral state in order to break negative habits. Pattern
+                  interrupts offer an effective way to break negative cycles and
+                  regain control over our actions, thoughts, and emotions. By
+                  recognizing our patterns, we can intervene and create new
+                  pathways for more productive thoughts, behaviors, and emotions
+                  and make lasting changes in our lives.
+                </CardDescription>
+              </CardContent>
+            </Card>
+            <Card className="shadow-none">
+              <CardContent className="space-y-2">
+                <CardTitle className="text-lg font-semibold">
+                  In Crisis? Need Help?
+                </CardTitle>
+                <CardDescription className="line-clamp-3 text-sm/6">
+                  In Crisis? Call or Text 988 Canada’s new suicide prevention
+                  hotline. If you or someone you know is in danger of hurting
+                  themselves or others, call 911 immediately. See our directory
+                  of mental health crisis support in your area below. Seek the
+                  advice of an appropriately qualified healthcare professional
+                  before making decisions about your circumstances.
+                </CardDescription>
+              </CardContent>
+            </Card>
+            <Card className="shadow-none">
+              <CardContent className="space-y-2">
+                <CardTitle className="text-lg font-semibold">
+                  The LifeLine Application
+                </CardTitle>
+                <CardDescription className="line-clamp-3 text-sm/6">
+                  The LifeLine App is a free Suicide Prevention and Awareness
+                  App that offers access and guidance to support for those
+                  suffering in crisis, and those who have suffered the
+                  devastating loss of a loved one from suicide, as well as for
+                  those struggling with any degree of anxiety or depression. The
+                  LifeLine App also provides awareness, education and prevention
+                  strategies to guide people in crisis.
+                </CardDescription>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="">
+            <Button>View More</Button>
           </div>
         </section>
       </main>
