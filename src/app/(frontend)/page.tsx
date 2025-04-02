f6c67718578ea76b7a584086a516e188bae3e339
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-04-02 14:58:31 -0400

    Added call to action

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -164,6 +164,64 @@ export default function Home() {
             <Button>View More</Button>
           </div>
         </section>
+
+        {/* Call To Action - Get Involved With Us. */}
+        <div className="relative rounded-3xl bg-primary/10 overflow-hidden">
+          <div className="relative h-80 overflow-hidden bg-primary/60 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
+            <img
+              className="size-full object-cover"
+              src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply"
+              alt=""
+            />
+            <svg
+              viewBox="0 0 926 676"
+              aria-hidden="true"
+              className="absolute -bottom-24 left-24 w-[57.875rem] transform-gpu blur-[118px]"
+            >
+              <path
+                className="fill-primary"
+                fill-opacity=".4"
+                d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z"
+              />
+              <defs>
+                <linearGradient
+                  id="60c3c621-93e0-4a09-a0e6-4c228a0116d8"
+                  x1="926.392"
+                  x2="-109.635"
+                  y1=".176"
+                  y2="321.024"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#776FFF" />
+                  <stop offset="1" stop-color="#FF4694" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+          <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
+            <div className="pr-6 pl-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pr-0 lg:pl-24 xl:pl-32">
+              <h2 className="text-base/7 font-semibold text-primary">
+                Get Involved! Change a Life!
+              </h2>
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-foreground capitalize sm:text-5xl">
+                Volunteering with TLC is rewarding and fun!
+              </p>
+              <p className="mt-6 text-base/7 text-foreground">
+                Thank you for your interest in volunteering for The LifeLine
+                Canada Foundation If you’d like to get involved, we are always
+                looking for dedicated and committed volunteers for short, long
+                term and task specific help. There are many opportunities
+                available. You can use the volunteer form below to share more
+                information regarding your interests and availability.
+              </p>
+              <div className="mt-8">
+                <Link href="#">
+                  <Button>Get Involved</Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
