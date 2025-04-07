cb22ff4d2ac8d30abcb06dc9b13f11eb75fa3eb0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-07 14:05:49 -0400

    Added basic content to the webpage.

--- src/app/(frontend)/about-us/page.tsx
+++ src/app/(frontend)/about-us/page.tsx
@@ -4,6 +4,23 @@ export default function page() {
   return (
     <main className="container mx-auto min-h-dvh px-6 lg:py-24">
       <h1 className="">Hello, About.</h1>
+      <p className="">Our strength is collaboration</p>
+      <p className="">
+        We believe that our strength lies in our collaborative approach, which
+        puts our clients at the center of everything we do.
+      </p>
+      <p className="">
+        Studio was started by three friends who noticed that developer studios
+        were charging clients double what an in-house team would cost. Since the
+        beginning, we have been committed to doing things differently by
+        charging triple instead.
+      </p>
+      <p className="">
+        At Studio, we’re more than just colleagues — we’re a family. This means
+        we pay very little and expect people to work late. We want our employees
+        to bring their whole selves to work. In return, we just ask that they
+        keep themselves there until at least 6:30pm.
+      </p>
     </main>
   );
 }
