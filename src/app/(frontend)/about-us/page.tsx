c6e83934a2513e6b565181015984562fcfc07ded
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-07 14:21:41 -0400

    Updated styles of the website

--- src/app/(frontend)/about-us/page.tsx
+++ src/app/(frontend)/about-us/page.tsx
@@ -3,20 +3,22 @@ import React from "react";
 export default function page() {
   return (
     <main className="container mx-auto min-h-dvh px-6 lg:py-24">
-      <section className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none text-balance">
-        <h1 className="">Hello, About.</h1>
-        <p className="">Our strength is collaboration</p>
-        <p className="">
+      <section className="mx-auto max-w-3xl lg:mx-0 text-balance space-y-4">
+        <h1 className="text-lg font-semibold">About Us.</h1>
+        <p className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl capitalize">
+          Our strength is collaboration
+        </p>
+        <p className="text-xl text-muted-foreground">
           We believe that our strength lies in our collaborative approach, which
           puts our clients at the center of everything we do.
         </p>
-        <p className="">
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
           Studio was started by three friends who noticed that developer studios
           were charging clients double what an in-house team would cost. Since
           the beginning, we have been committed to doing things differently by
           charging triple instead.
         </p>
-        <p className="">
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
           At Studio, we’re more than just colleagues — we’re a family. This
           means we pay very little and expect people to work late. We want our
           employees to bring their whole selves to work. In return, we just ask
