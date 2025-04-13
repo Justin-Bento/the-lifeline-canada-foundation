8e125dc2ccf6f4941808e48d3bf05b150257e2d0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-04-13 16:13:59 -0400

    Added a custom bento grid to go over ponts areas the lifline can hellp you with.
    We're utilizing the bento grids to isolate each issue emphasizing that each isssue is unique and to the individual.

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,19 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CircleAlert, CornerDownRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <main className="container mx-auto min-h-dvh space-y-64 py-24  lg:px-6">
+      <main className="container mx-auto min-h-dvh space-y-40 py-40 px-4  lg:px-6">
         <section className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           <div className="relative w-full aspect-[16/10]">
             <Image
@@ -56,138 +64,201 @@ export default function Home() {
         </section>
 
         {/* Products / Services Section */}
-        <section className="mx-auto max-w-2xl px-6 lg:container lg:mx-auto lg:px-6">
+        <section className="mx-auto lg:container lg:mx-auto lg:px-6">
           <div className="space-y-4">
-            <h2 className="text-base/7 font-semibold text-indigo-600">
+            <h2 className="mt-2 capitalize text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
               Where Here to Support You!
             </h2>
-            <p className="mt-2 capitalize text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
-              We&#39;re not a crisis hotline, but we&#39;re here to help in any
-              way we can.
-            </p>
             <p className="mt-4 max-w-3xl text-balance">
               While we may not be equipped for emergency situations, we can
               connect you with the resources and support you need. How can we
               assist you today? We&#39;re here for you.
             </p>
           </div>
-          <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
-            <div className="relative lg:row-span-2">
-              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
-                <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                    Mental Health Resources.
-                  </p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo.
-                  </p>
-                  <Link
-                    href="#"
-                    className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center"
-                  >
-                    <span className="absolou"></span>
-                    Read More
-                  </Link>
-                </div>
-                <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
-                  <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                    <div className="relative size-full">
-                      <Image
-                        fill
-                        className="object-cover object-top"
-                        src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
-            </div>
-            <div className="relative max-lg:row-start-1">
-              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                    Pattern Interupts
-                  </p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit
-                    maiores impedit.
-                  </p>
-                </div>
-                <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
-                  <div className="relative w-full max-lg:max-w-xs">
-                    <Image
-                      fill
-                      className="w-full max-lg:max-w-xs"
-                      src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-performance.png"
-                      alt=""
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
-            </div>
-            <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-              <div className="absolute inset-px rounded-lg bg-white"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
-                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                    In Crisis? Need Help?
-                  </p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                    suspendisse semper morbi.
-                  </p>
-                </div>
-                <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
-                  <div className="relative h-[min(152px,40cqw)]">
-                    <Image
-                      className="object-cover"
-                      src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-security.png"
-                      alt=""
-                      fill
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
-            </div>
-            <div className="relative lg:row-span-2">
-              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-                <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                    Powerful APIs
-                  </p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Sit quis amet rutrum tellus ullamcorper ultricies libero
-                    dolor eget sem sodales gravida.
-                  </p>
-                </div>
-                <div className="relative min-h-[30rem] w-full grow">
-                  <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                      <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                        <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                          NotificationSetting.jsx
-                        </div>
-                        <div className="border-r border-gray-600/10 px-4 py-2">
-                          App.jsx
-                        </div>
-                      </div>
-                    </div>
-                    <div className="px-6 pt-6 pb-14">
-                      {/* Your code example */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-            </div>
+          <div className="grid grid-cols-3 gap-12 mt-12">
+            <Card className="col-span-3 aspect-video lg:col-span-2 relative overflow-hidden">
+              <Image
+                src="/media/image/landing-page-2.jpg"
+                alt=""
+                fill
+                className="object-cover"
+              />
+            </Card>
+            <Card className="col-span-3 lg:h-full lg:col-span-1 flex items-center justify-center  aspect-square">
+              <CardContent className="">
+                <CircleAlert className="my-4" />
+                <CardTitle className="text-xl capitalize text-balance">
+                  We&#39;re not a crisis hotline, but we&#39;re here to help in
+                  any way we can.
+                </CardTitle>
+                <CardDescription className="text-base/relaxed text-balance mt-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima enim nam similique inventore vel ipsam error illum
+                  reprehenderit earum neque commodi soluta omnis magni ratione
+                  vitae, sit consequuntur dignissimos sequi?
+                </CardDescription>
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="pl-0 text-xs text-muted-foreground"
+                >
+                  view page &rarr;
+                </Button>
+              </CardContent>
+            </Card>
+            <Card className="col-span-3 lg:col-span-1 group relative isolate shadow-none pt-0 overflow-hidden">
+              <CardHeader className="relative w-full aspect-video bg-gray-200">
+                <Image
+                  fill
+                  src="/media/image/landing-image-3.png"
+                  alt=""
+                  className="object-cover "
+                />
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Link href="#">
+                  <span className="absolute inset-0 hover:bg-gray-200/10"></span>
+                  <CardTitle className="group-hover:underline mb-2">
+                    Depression
+                  </CardTitle>
+                </Link>
+                <CardDescription>
+                  Sadness is a natural part of being human, find ways to cope.
+                </CardDescription>
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="pl-0 text-xs text-muted-foreground"
+                >
+                  view page &rarr;
+                </Button>
+              </CardContent>
+            </Card>
+            <Card className="col-span-3 lg:col-span-1 group relative isolate shadow-none pt-0 overflow-hidden">
+              <CardHeader className="relative w-full aspect-video bg-gray-200">
+                <Image
+                  fill
+                  src="/media/image/landingPage/landing-image-4.jpg"
+                  alt=""
+                  className="object-cover"
+                />
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Link href="#">
+                  <span className="absolute inset-0 hover:bg-gray-200/10"></span>
+                  <CardTitle className="group-hover:underline mb-2">
+                    Anxiety
+                  </CardTitle>
+                </Link>
+                <CardDescription>
+                  Resources to help with feeling of fear, worry, and uneasiness.
+                </CardDescription>
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="pl-0 text-xs text-muted-foreground"
+                >
+                  view page &rarr;
+                </Button>
+              </CardContent>
+            </Card>
+            <Card className="col-span-3 lg:col-span-1 group relative isolate shadow-none pt-0 overflow-hidden">
+              <CardHeader className="relative w-full aspect-video bg-gray-200">
+                <Image
+                  fill
+                  src="/media/image/landingPage/landing-image-5.png"
+                  alt=""
+                  className="object-cover"
+                />
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Link href="#">
+                  <span className="absolute inset-0 hover:bg-gray-200/10"></span>
+                  <CardTitle className="group-hover:underline mb-2">
+                    Grief Support
+                  </CardTitle>
+                </Link>
+                <CardDescription>
+                  Need help with the loss of a loved one? You’re not alone.
+                </CardDescription>
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="pl-0 text-xs text-muted-foreground"
+                >
+                  view page &rarr;
+                </Button>
+              </CardContent>
+            </Card>
+            <Card className="col-span-3 lg:col-span-1 group relative isolate shadow-none pt-0 overflow-hidden">
+              <CardHeader className="relative w-full aspect-video bg-gray-200">
+                <Image
+                  fill
+                  src="/media/image/landingPage/landing-image-6.png"
+                  alt=""
+                  className="object-cover"
+                />
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Link href="#">
+                  <span className="absolute inset-0 hover:bg-gray-200/10"></span>
+                  <CardTitle className="group-hover:underline mb-2">
+                    Crisis Centers
+                  </CardTitle>
+                </Link>
+                <CardDescription>
+                  Connect with caring people for online therapy & counseling.
+                </CardDescription>
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="pl-0 text-xs text-muted-foreground"
+                >
+                  view page &rarr;
+                </Button>
+              </CardContent>
+            </Card>
+            <Card className="col-span-3 lg:col-span-1 group relative isolate shadow-none pt-0 overflow-hidden">
+              <CardHeader className="relative w-full aspect-video bg-gray-200">
+                <Image
+                  fill
+                  src="/media/image/landingPage/landing-image-7.png"
+                  alt=""
+                  className="object-cover"
+                />
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Link href="#">
+                  <span className="absolute inset-0 hover:bg-gray-200/10"></span>
+                  <CardTitle className="group-hover:underline mb-2">
+                    E-Counselling
+                  </CardTitle>
+                </Link>
+                <CardDescription>
+                  Advice and tools to help guide parents through their journey.
+                </CardDescription>
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="pl-0 text-xs text-muted-foreground"
+                >
+                  view page &rarr;
+                </Button>
+              </CardContent>
+            </Card>
+            <Card className="col-span-3 lg:col-span-1 group relative isolate shadow-none pt-0 overflow-hidden">
+              <CardHeader className="w-full aspect-video grid place-content-center">
+                <CornerDownRight className="size-40" />
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Link href="#">
+                  <span className="absolute inset-0 group-hover:cursor-pointer group-hover:bg-purple-400/50 -z-10"></span>
+                  <CardTitle className="group-hover:underline mb-2 text-center">
+                    Click Here to Continue Reading
+                  </CardTitle>
+                </Link>
+              </CardContent>
+            </Card>
           </div>
         </section>
 
