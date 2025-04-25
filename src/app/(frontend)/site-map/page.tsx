d99029786242e11906abe4dda99b26bf6245a164
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-25 15:19:28 -0400

    Removed extra container

--- src/app/(frontend)/site-map/page.tsx
+++ src/app/(frontend)/site-map/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function page() {
   return (
-    <main className="container mx-auto min-h-dvh px-6 lg:py-24">
+    <main className="min-h-dvh lg:py-24">
       <Wrapper>
         <h1 className="">Hello, Site Map Page.</h1>
       </Wrapper>
