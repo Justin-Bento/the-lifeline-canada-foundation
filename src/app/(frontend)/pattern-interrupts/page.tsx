9a312251c47313a36fce729866e76c1365fd91f8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-26 15:58:18 -0400

    Updated layout for pattern interupts page

--- src/app/(frontend)/pattern-interrupts/page.tsx
+++ src/app/(frontend)/pattern-interrupts/page.tsx
@@ -1,11 +1,36 @@
-import Wrapper from "@/components/Wrapper";
 import React from "react";
+import Image from "next/image";
+import Wrapper from "@/components/Wrapper";
 
 export default function page() {
   return (
-    <main className="container mx-auto min-h-dvh px-6 lg:py-24">
-      <Wrapper>
-        <h1 className="">Hello, Pattern Interupts?</h1>
+    <main className="space-y-8 mb-16 min-h-dvh">
+      <div className="relative w-full h-96">
+        <Image
+          fill
+          alt=""
+          src="/media/image/image-2.png"
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-white" />
+      </div>
+      <Wrapper className="mb-24">
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+          Pattern Interruptions
+        </h1>
+        <span className="block max-w-3xl text-balance space-y-8 pt-8">
+          <p className="">
+            A Pattern Interrupt is a way to alter our mental, emotional, or
+            behavioral state in order to break negative habits. Pattern
+            interrupts offer an effective way to break negative cycles and
+            regain control over our actions, thoughts, and emotions.
+          </p>
+          <p className="">
+            By recognizing our patterns, we can intervene and create new
+            pathways for more productive thoughts, behaviors, and emotions and
+            make lasting changes in our lives.
+          </p>
+        </span>
       </Wrapper>
     </main>
   );
