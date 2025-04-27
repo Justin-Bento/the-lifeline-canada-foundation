246bbc19a3ca65f1e34491afb67ab9a7d56238c6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-27 19:56:03 -0400

    Wrapped p tags inside a span and applied an arbitrary variants ensuring constent typography within the sub heading

--- src/app/(frontend)/about-us/page.tsx
+++ src/app/(frontend)/about-us/page.tsx
@@ -23,29 +23,31 @@ export default function page() {
       <Wrapper className="min-h-dvh lg:py-24 space-y-24">
         <section className="mx-auto max-w-3xl lg:mx-0 text-balance space-y-4">
           <h1 className="text-lg font-semibold">About Us.</h1>
-          <p className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl capitalize">
-            Our strength is our Commitment
-          </p>
-          <p className="text-xl text-muted-foreground">
-            The LifeLine Canada Foundation (or TLC) is a registered non-profit,
-            committed to positive mental health and suicide prevention &
-            awareness and is constantly developing new initiatives in support of
-            these goals.
-          </p>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            The LifeLine Canada Foundation was started To aid in suicide
-            reduction across Canada by raising awareness of risk factors,
-            delivering programs for positive mental health development,
-            providing access to available resource options, and promoting local
-            programs to build mental health resilience for all. .
-          </p>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            At The Lifeline Canada Foundation, we’re more than just enthusiastic
-            volunteers — we’re make a significant difference, by raising
-            awareness to continue to lower the risk of suicide to where it is a
-            rare event across the country and to build mental health resilience
-            for all.
-          </p>
+          <span className="block [&>p]:text-balance [&>p]:leading-relax space-y-4">
+            <p className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl capitalize mb-8">
+              Our strength is our Commitment
+            </p>
+            <p className="text-xl">
+              The LifeLine Canada Foundation (or TLC) is a registered
+              non-profit, committed to positive mental health and suicide
+              prevention & awareness and is constantly developing new
+              initiatives in support of these goals.
+            </p>
+            <p>
+              The LifeLine Canada Foundation was started To aid in suicide
+              reduction across Canada by raising awareness of risk factors,
+              delivering programs for positive mental health development,
+              providing access to available resource options, and promoting
+              local programs to build mental health resilience for all. .
+            </p>
+            <p>
+              At The Lifeline Canada Foundation, we’re more than just
+              enthusiastic volunteers — we’re make a significant difference, by
+              raising awareness to continue to lower the risk of suicide to
+              where it is a rare event across the country and to build mental
+              health resilience for all.
+            </p>
+          </span>
         </section>
         <section className="">
           <h2 className="text-3xl font-semibold tracking-tight text-foreground sm:text-4xl capitalize">
