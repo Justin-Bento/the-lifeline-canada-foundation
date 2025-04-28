c6ee54edef186b45f2d928be6489347e80b52458
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-28 16:16:54 -0400

    Created crisis center page

---
+++ src/app/(frontend)/in-crisis/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import Wrapper from "@/components/Wrapper";
+import React from "react";
+
+export default function page() {
+  return (
+    <Wrapper layout="fullHeight" spacing="medium">
+      <h1>Canadian Crisis Centres</h1>
+    </Wrapper>
+  );
+}
