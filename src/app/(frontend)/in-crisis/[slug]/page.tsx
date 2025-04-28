0c1871b3d4a520ac6425b72237080f4c28a6504b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-28 16:17:14 -0400

    added paragraphs from main website to current page.

--- src/app/(frontend)/in-crisis/[slug]/page.tsx
+++ src/app/(frontend)/in-crisis/[slug]/page.tsx
@@ -5,6 +5,26 @@ export default function page() {
   return (
     <Wrapper layout="fullHeight" spacing="medium">
       <h1>Canadian Crisis Centres</h1>
+      <span>
+        <p>
+          NEW! Canada’s 988 hotline, gives people access to suicide prevention
+          services via call or text.
+        </p>
+        <p>
+          People in every province and territory who are experiencing a mental
+          health crisis and need immediate, real-time support can use the
+          three-digit number. Similar to 911 for accessing fire, police and
+          medical emergencies, 988: Suicide Crisis Helpline is a short,
+          easy-to-remember number to get a quick response from coast to coast to
+          coast, 24/7 and free of charge.
+        </p>
+        <p>
+          Calls and texts to 988 will be directed to a network of partners in
+          communities across the country offering suicide prevention services
+          such as counselling. The 988 service is federally funded and available
+          in English and French.
+        </p>
+      </span>
     </Wrapper>
   );
 }
