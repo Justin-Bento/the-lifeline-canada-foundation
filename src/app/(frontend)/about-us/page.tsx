32211427dd2925f97b94a868844d2f74043551e1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-27 20:24:09 -0400

    Updated component to include the new layout directive

--- src/app/(frontend)/about-us/page.tsx
+++ src/app/(frontend)/about-us/page.tsx
@@ -20,7 +20,7 @@ export default function page() {
         />
         <div className="absolute inset-0 bg-linear-to-t from-white" />
       </div>
-      <Wrapper className="min-h-dvh lg:py-24 space-y-24">
+      <Wrapper layout="containerVH" className="lg:py-24 space-y-24 mb-24">
         <section className="mx-auto max-w-3xl lg:mx-0 text-balance space-y-4">
           <h1 className="text-lg font-semibold">About Us.</h1>
           <span className="block [&>p]:text-balance [&>p]:leading-relax space-y-4">
