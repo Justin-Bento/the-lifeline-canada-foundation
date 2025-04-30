2920d02dbb5eeb630f3937fdf17b600b21166fdf
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-30 13:52:50 -0400

    Added banner image to the contact us page for the webiste

--- src/app/(frontend)/contact-us/page.tsx
+++ src/app/(frontend)/contact-us/page.tsx
@@ -1,9 +1,20 @@
 import Wrapper from "@/components/Wrapper";
+import Image from "next/image";
 import React from "react";
 
 export default function page() {
   return (
     <main>
+      <div className="relative ">
+        <Image
+          alt="Group of actionable words about volenteering."
+          width={1920}
+          height={180}
+          src="/media/image/image-8.png"
+          className="object-cover object-center"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-white" />
+      </div>
       <Wrapper layout="fullHeight" whiteSpace="medium">
         <h1 className="">Hello, Contact Us Page.</h1>
       </Wrapper>
