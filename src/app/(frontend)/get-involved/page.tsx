45e1334f66f276b862b3b80d26783b6334f6b4d7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-30 13:51:23 -0400

    Coppied content from the TLC website on the volunteers page.

--- src/app/(frontend)/get-involved/page.tsx
+++ src/app/(frontend)/get-involved/page.tsx
@@ -16,7 +16,22 @@ export default function page() {
         <div className="absolute inset-0 bg-linear-to-t from-white" />
       </div>
       <Wrapper layout="fullHeight" whiteSpace="medium">
-        <h1 className="">Hello, Get Involved Page.</h1>
+        <section className="space-y-5">
+          <span className="flex flex-col-reverse gap-1">
+            <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+              Volunteering with TLC is rewarding and fun!
+            </h1>
+            <p className="text-sm text-purple-500 font-mono">Get Involved</p>
+          </span>
+          <p className="text-xl/8 leading-relaxed max-w-4xl text-balance">
+            We offer enthusiastic volunteers an opportunity to make a
+            significant difference in the lives of Canadians in keeping with our
+            vision: “To continue to lower the risk of suicide to where it is a
+            rare event across the country and to build mental health resilience
+            for all.” It is also an opportunity to gain important experience in
+            specific vocational areas and/or with the non-profit sector.
+          </p>
+        </section>
       </Wrapper>
     </main>
   );
