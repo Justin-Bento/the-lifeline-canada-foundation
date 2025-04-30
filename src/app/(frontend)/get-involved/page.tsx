cf8373b310a5742eb4faec5a65ead1763ec9b392
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-30 13:49:48 -0400

    Added hero banner for get inovolved page..

--- src/app/(frontend)/get-involved/page.tsx
+++ src/app/(frontend)/get-involved/page.tsx
@@ -1,9 +1,20 @@
-import Wrapper from "@/components/Wrapper";
 import React from "react";
+import Image from "next/image";
+import Wrapper from "@/components/Wrapper";
 
 export default function page() {
   return (
     <main>
+      <div className="relative ">
+        <Image
+          alt="Group of actionable words about volenteering."
+          width={1920}
+          height={180}
+          src="/media/image/image-7.jpg"
+          className="object-cover object-center"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-white" />
+      </div>
       <Wrapper layout="fullHeight" whiteSpace="medium">
         <h1 className="">Hello, Get Involved Page.</h1>
       </Wrapper>
