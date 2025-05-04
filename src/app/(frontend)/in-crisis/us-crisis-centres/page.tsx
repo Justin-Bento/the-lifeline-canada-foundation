e9e1c69773ee522716ca1498f4e8af6993a66663
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-04 19:56:37 -0400

    Created a  static route for us-crisis-centers

---
+++ src/app/(frontend)/in-crisis/us-crisis-centres/page.tsx
@@ -0,0 +1,10 @@
+import Wrapper from "@/components/Wrapper";
+import React from "react";
+
+export default function page() {
+  return (
+    <Wrapper height="screen" spacing="md">
+      <h1 className="">Hello, United States Crisis Center Page.</h1>
+    </Wrapper>
+  );
+}
