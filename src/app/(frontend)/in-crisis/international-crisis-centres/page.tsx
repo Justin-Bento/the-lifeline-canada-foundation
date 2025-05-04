5dc36d4522caf87b01e7f4df9f2472bc514b0a1a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-04 19:58:46 -0400

    Created a static route called "international crisis center.

---
+++ src/app/(frontend)/in-crisis/international-crisis-centres/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Wrapper from "@/components/Wrapper";
+
+export default function page() {
+  return (
+    <Wrapper height="screen" spacing="md">
+      <h1 className="">Hello, InternationalCrisis Center Page.</h1>
+    </Wrapper>
+  );
+}
