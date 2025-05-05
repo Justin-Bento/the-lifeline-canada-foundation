e2870dd2f6dfcf67a3873a6b71bceebb2e116aa1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-05 13:21:46 -0400

    Turned the page heading of the "The Lifeline App" into a react component.
    This ensures page headings look identcal throughout the interface.
    
    Have a consistent flow and infromation.
    
    Easily update multpule pages throughout the interface without having to go through each page.

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+import Wrapper from "./Wrapper";
+
+export default function PageHeader() {
+  return (
+    <>
+      <div className="relative w-full h-96">
+        <Image
+          fill
+          alt=""
+          src="/media/image/image-4.png"
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-white" />
+      </div>
+      <Wrapper
+        width="container"
+        height="screen"
+        spacing="lg"
+        className="space-y-24"
+      >
+        <div className="">
+          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+            The LifeLine Mobile App
+          </h1>
+          <span className="block max-w-4xl text-balance space-y-8 pt-8">
+            <p className="text-xl/8 text-muted-foreground leading-snug">
+              The LifeLine App is the National free Suicide Prevention and
+              Awareness App that offers access and guidance to support for those
+              suffering in crisis and those who have suffered the devastating
+              loss of a loved one from suicide. The LifeLine App also provides
+              awareness education and prevention strategies to guide people in
+              crisis all across the Globe. The App was developed as a
+              centralized hub to connect people with accredited resources in
+              Canada and throughout the world.
+            </p>
+          </span>
+        </div>
+      </Wrapper>
+    </>
+  );
+}
