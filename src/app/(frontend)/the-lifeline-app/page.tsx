c171a2064c694eae4701b6dbb1ac3e8a113752ad
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-04 22:24:20 -0400

    Wrapped the page header inside a div. Preventing elements from being distant.

--- src/app/(frontend)/the-lifeline-app/page.tsx
+++ src/app/(frontend)/the-lifeline-app/page.tsx
@@ -20,21 +20,23 @@ export default function page() {
         spacing="lg"
         className="space-y-24"
       >
-        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
-          The LifeLine Mobile App
-        </h1>
-        <span className="block max-w-4xl text-balance space-y-8 pt-8">
-          <p className="text-xl/8 leading-snug">
-            The LifeLine App is the National free Suicide Prevention and
-            Awareness App that offers access and guidance to support for those
-            suffering in crisis and those who have suffered the devastating loss
-            of a loved one from suicide. The LifeLine App also provides
-            awareness education and prevention strategies to guide people in
-            crisis all across the Globe. The App was developed as a centralized
-            hub to connect people with accredited resources in Canada and
-            throughout the world.
-          </p>
-        </span>
+        <div className="">
+          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+            The LifeLine Mobile App
+          </h1>
+          <span className="block max-w-4xl text-balance space-y-8 pt-8">
+            <p className="text-xl/8 leading-snug">
+              The LifeLine App is the National free Suicide Prevention and
+              Awareness App that offers access and guidance to support for those
+              suffering in crisis and those who have suffered the devastating
+              loss of a loved one from suicide. The LifeLine App also provides
+              awareness education and prevention strategies to guide people in
+              crisis all across the Globe. The App was developed as a
+              centralized hub to connect people with accredited resources in
+              Canada and throughout the world.
+            </p>
+          </span>
+        </div>
       </Wrapper>
     </>
   );
