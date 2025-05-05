842599915931de40cdb8cc2de7699b439e6d71b0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-05 13:48:47 -0400

    Updated contact page with the new Page Header Component

--- src/app/(frontend)/contact-us/page.tsx
+++ src/app/(frontend)/contact-us/page.tsx
@@ -1,40 +1,24 @@
-import Wrapper from "@/components/Wrapper";
-import Image from "next/image";
 import React from "react";
+import PageHeader from "@/components/PageHeader";
 
 export default function page() {
+  let content = {
+    media: {
+      URL: "/media/image/image-8.png", // Can be string URL or StaticImageData
+      ALT: "Group of actionable words about volenteering.",
+    },
+    title: "Contact Us",
+    description: `For non crisis questions and comments or to contact The LifeLine Canada Foundation about collaborating please use the contact form below.`,
+  };
   return (
-    <main>
-      <div className="relative ">
-        <Image
-          alt="Group of actionable words about volenteering."
-          width={1920}
-          height={180}
-          src="/media/image/image-8.png"
-          className="object-cover object-center"
+    <>
+      <div className="">
+        <PageHeader
+          Banner={content.media}
+          Title={content.title}
+          Description={content.description}
         />
-        <div className="absolute inset-0 bg-linear-to-t from-white" />
       </div>
-      <Wrapper
-        width="container"
-        height="screen"
-        spacing="md"
-        className="space-y-24"
-      >
-        <section className="space-y-5">
-          <span className="flex flex-col-reverse gap-1">
-            <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
-              Contact Us
-            </h1>
-            <p className="text-sm text-purple-500 font-mono">Get Involved</p>
-          </span>
-          <p className="text-xl/8 leading-relaxed max-w-4xl text-balance">
-            For non crisis questions and comments or to contact The LifeLine
-            Canada Foundation about collaborating please use the contact form
-            below.
-          </p>
-        </section>
-      </Wrapper>
-    </main>
+    </>
   );
 }
