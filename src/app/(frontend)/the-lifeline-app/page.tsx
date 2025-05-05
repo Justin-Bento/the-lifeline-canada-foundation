d8431a438392d1b195cc8a49d39a6b08800205b4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-05 13:46:58 -0400

    Created an object called content to store the page description of the website.

--- src/app/(frontend)/the-lifeline-app/page.tsx
+++ src/app/(frontend)/the-lifeline-app/page.tsx
@@ -2,10 +2,22 @@ import React from "react";
 import PageHeader from "@/components/PageHeader";
 
 export default function page() {
+  let content = {
+    media: {
+      URL: "/media/image/image-4.png", // Can be string URL or StaticImageData
+      ALT: "King sitting on throne",
+    },
+    title: "The LifeLine Mobile App",
+    description: `The LifeLine App is the National free Suicide Prevention and Awareness App that offers access and guidance to support for those suffering in crisis and those who have suffered the devastating loss of a loved one from suicide. The LifeLine App also provides awareness education and prevention strategies to guide people in crisis all across the Globe. The App was developed as a centralized hub to connect people with accredited resources in Canada and throughout the world.`,
+  };
   return (
     <>
       <div className="">
-        <PageHeader />
+        <PageHeader
+          Banner={content.media}
+          Title={content.title}
+          Description={content.description}
+        />
       </div>
     </>
   );
