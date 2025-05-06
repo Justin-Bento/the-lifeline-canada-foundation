8544c5f99be8c78cb58fbc53ccc659b27f5af69b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-06 17:29:04 -0400

    Created a new route called 'projects using Next.js file-system based router built on the concept of pages.

---
+++ src/app/(frontend)/projects/page.tsx
@@ -0,0 +1,34 @@
+import { PageHeader } from "@/components/PageHeader";
+import Wrapper from "@/components/Wrapper";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <PageHeader>
+        <PageHeader.Banner
+          image="/media/image/image-1.png"
+          alt="Group of actionable words about volenteering."
+        />
+        <PageHeader.Container>
+          <PageHeader.Headline className="flex flex-col-reverse">
+            See How We&#39;re Making a Difference!
+            <span className="text-xs font-medium tracking-wide text-purple-500 m-0 p-0">
+              TLC Projects
+            </span>
+          </PageHeader.Headline>
+          <PageHeader.Description>
+            From community outreach to large-scale initiatives, each of our
+            projects is a deliberate step toward a brighter, more equitable
+            future. Through education, empowerment programs, we&#39;re creating
+            measurable change by being a central hub for all things mental
+            health.
+          </PageHeader.Description>
+        </PageHeader.Container>
+      </PageHeader>
+      <Wrapper height="screen" spacing="lg">
+        <h1 className="">The LifeLine Mobile App</h1>
+      </Wrapper>
+    </>
+  );
+}
