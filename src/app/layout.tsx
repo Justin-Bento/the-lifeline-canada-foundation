a8fe4f2bcf8aac590579c269c210ca6a20150db8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-11-02 22:58:50 -0500

    Removed cookies function as it was declared but never used.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavigationTop from "@/components/NavigationTop";
 import NavigationBottom from "@/components/NavigationBottom";
-import { cookies } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
