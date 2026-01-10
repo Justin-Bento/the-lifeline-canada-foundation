d3152eed049d4bcce42c82da72ba50303b115b6a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2026-01-10 15:11:22 -0400

    Updated id to be unique by adding 1.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,37 +1,8 @@
 export const top_navigation = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "About",
-    url: "/about-us",
-  },
-  {
-    id: 3,
-    title: "About",
-    url: "/about-us",
-  },
-  {
-    id: 4,
-    title: "Resources",
-    url: "/resources",
-  },
-  {
-    id: 4,
-    title: "Patterns",
-    url: "/pattern-interrupts",
-  },
-  {
-    id: 4,
-    title: "Crisis Centers",
-    url: "/in-crisis-need-help",
-  },
-  {
-    id: 4,
-    title: "Contact",
-    url: "/contact",
-  },
+  { id: 1, title: "Home", url: "/" },
+  { id: 2, title: "About", url: "/about-us" },
+  { id: 3, title: "Resources", url: "/resources" },
+  { id: 4, title: "Patterns", url: "/pattern-interrupts" },
+  { id: 5, title: "Crisis Centers", url: "/in-crisis-need-help" },
+  { id: 6, title: "Contact", url: "/contact" },
 ];
