50e7cf31ec383074beaeb6817b737ad1c39cee06
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2026-01-10 15:06:47 -0400

    Updated data to include third party data outside.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,177 +1,37 @@
-export const about_page_page_info = {
-  heading: "Welcome, About Us!",
-  overview: [
-    "The LifeLine Canada Foundation (or TLC) is a registered non-profit, committed to positive mental health and suicide prevention & awareness and is constantly developing new initiatives in support of these goals.",
-    "The LifeLine Canada Foundation is not a crisis hotline. If in Crisis, call or text 988 for Canada’s new suicide prevention hotline. If you or someone you know is in danger of hurting themselves or others, call 911 immediately. See our directory of mental health crisis support in your area.",
-  ],
-  media: {
-    path: "/media/company-banner.jpg",
-    description: "/media/company-banner.jpg",
-  },
-};
-export const contact_page_info = {
-  heading: "Contact Us!",
-  overview: [
-    "For non crisis questions and comments or to contact The LifeLine Canada Foundation about collaborating please use the contact form below.",
-    "The LifeLine Canada Foundation (TLC) is a registered non-profit committed to the prevention of suicide and positive mental health in Canada and Worldwide and has made work in this area a top priority. We envision a country where suicide is a rare event, where people are nurtured and supported, where individuals and families are aware of the risk factors for suicide and actively seek help from accessible, effective community resources.",
-  ],
-  media: {
-    path: "/media/company-banner.jpg",
-    description: "/media/company-banner.jpg",
-  },
-};
-export const in_crisis_page_info = {
-  heading: "In Crisis? Need Help?",
-  overview: [
-    "In Crisis? Call or Text 988 Canada’s new suicide prevention hotline. If you or someone you know is in danger of hurting themselves or others, call 911 immediately. See our directory of  mental health crisis support in your area below.",
-    "Seek the advice of an appropriately qualified healthcare professional before making decisions about your circumstances.",
-  ],
-  media: {
-    path: "/media/company-banner.jpg",
-    description: "/media/company-banner.jpg",
-  },
-};
-export const in_crisis = {
-  canada: {
-    id: 1,
-    title: "Canada",
-    overview:
-      "Canada is a country in North America. Its ten provinces and three territories.",
-  },
-  united_states: {
-    id: 1,
-    title: "United States of America (USA)",
-    overview:
-      "Canada is a country in North America. Its ten provinces and three territories.",
-  },
-  international: {
-    id: 1,
-    title: "International",
-    overview:
-      "Canada is a country in North America. Its ten provinces and three territories.",
-  },
-};
-export const pattern_interrupts_page_info = {
-  heading: "Pattern Interruptions",
-  overview: [
-    "A Pattern Interrupt is a way to alter our mental, emotional, or behavioral state in order to break negative habits. Pattern interrupts offer an effective way to break negative cycles and regain control over our actions, thoughts, and emotions.",
-    "By recognizing our patterns, we can intervene and create new pathways for more productive thoughts, behaviors, and emotions and make lasting changes in our lives.",
-  ],
-  media: {
-    path: "/media/company-banner.jpg",
-    description: "/media/company-banner.jpg",
-  },
-};
-export const projects_page_info = {
-  heading: "Get Involved!",
-  overview: [
-    "We offer enthusiastic volunteers an opportunity to make a significant difference in the lives of Canadians in keeping with our vision: “To continue to lower the risk of suicide to where it is a rare event across the country and to build mental health resilience for all.” It is also an opportunity to gain important experience in specific vocational areas and/or with the non-profit sector.",
-  ],
-  media: {
-    path: "/media/company-banner.jpg",
-    description: "/media/company-banner.jpg",
-  },
-};
-export const pattern_interruptions = [
+export const top_navigation = [
   {
     id: 1,
-    title: "improve your mood",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
+    title: "Home",
+    url: "/",
   },
   {
     id: 2,
-    title: "comedy",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
+    title: "About",
+    url: "/about-us",
   },
   {
     id: 3,
-    title: "inspiring videos",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
+    title: "About",
+    url: "/about-us",
   },
   {
     id: 4,
-    title: "emotional support videos",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
+    title: "Resources",
+    url: "/resources",
   },
   {
-    id: 5,
-    title: "uplifting music videos",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
+    id: 4,
+    title: "Pattern Interruptions",
+    url: "/pattern-interrupts",
   },
   {
-    id: 6,
-    title: "life changing books",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
+    id: 4,
+    title: "Crisis Centers",
+    url: "/in-crisis-need-help",
   },
   {
-    id: 7,
-    title: "helpful movies",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 8,
-    title: "movies of inspiration",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 9,
-    title: "self management",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 10,
-    title: "coping mechanisms",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 11,
-    title: "interactive mind",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 12,
-    title: "mental health apps",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 13,
-    title: "transform negative thoughts",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 14,
-    title: "words of wisdom",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
-  },
-  {
-    id: 15,
-    title: "image gallery",
-    overview:
-      "Suicidal thoughts can happen when feelings of hopelessness, isolation and despair become too heavy to bear. You might feel so overwhelmed with pain that suicide seems like the only way to release yourself from the burden you’ve been carrying. You should know that there is help available to deal with your feelings. Contacting a mental health professional can help you heal and experience joy and happiness again, no matter how impossible it seems right now.",
+    id: 4,
+    title: "Contact",
+    url: "/contact",
   },
 ];
-export const resources_page_info = {
-  heading: "Resources",
-  overview: [
-    "In Crisis? Call or Text 988 Canada’s new suicide prevention hotline. If you or someone you know is in danger of hurting themselves or others, call 911 immediately. See our directory of mental health crisis support in your area.",
-    "This site provides information about anxiety, depression and suicide. Online resources alone are not a substitute for professional medical advice, diagnosis or treatment. Seek the advice of an appropriately qualified healthcare professional before making decisions about your circumstances.",
-  ],
-  media: {
-    path: "/media/company-banner.jpg",
-    description: "/media/company-banner.jpg",
-  },
-};
